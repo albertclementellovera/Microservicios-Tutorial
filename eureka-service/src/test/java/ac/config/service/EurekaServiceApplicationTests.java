537b074af9525b1ddf8e0246b42193f83d468a03
albert <albertclementellovera@gmail.com>
2022-11-01 22:14:19 -0600

    eureka

---
+++ eureka-service/src/test/java/ac/config/service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- moto-service/src/main/java/ac/spring/moto/service/MotoServiceApplication.java
+++ moto-service/src/main/java/ac/spring/moto/service/MotoServiceApplication.java
@@ -2,8 +2,11 @@ package ac.spring.moto.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
